14c55baabef027d94cff6be679b5d389f7316f1f
jujuub2 <96797831+jujuub2@users.noreply.github.com>
2022-12-22 13:47:09 +0100

    Add files via upload

---
+++ aa.js
@@ -0,0 +1,20 @@
+var x =0;
+var dx = 2;
+function setup() {
+  /*remplacer le createCanvas, par les deux lignes suivantes (evidement vous choissez la taille du canevas !*/
+  var canvas = createCanvas(100, 100);
+  canvas.parent('conteneur_de_mon_Sketch');
+
+  background(255, 0, 200);
+}
+
+function draw(){
+   background(255, 0, 200);
+  x=x+dx;
+  ellipse (x,width/2,10,10);
+  if (x>width || x<0){
+    dx=-dx;
+  }
+  
+}
+
